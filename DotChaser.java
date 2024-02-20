bb4ff6cc0dfe3062966c0e42f1f613d92f6b11f3
Zephyr2 <zephyr2@Zephyr-2.local>
2024-02-20 17:16:28 -0500

    old commented code removed

--- DotChaser.java
+++ DotChaser.java
@@ -10,9 +10,7 @@ public class DotChaser {
     ThingList list = new ThingList();
     Random rand = new Random(System.currentTimeMillis());
 
-    // INSTEAD OF A NODE, CREATE SOMETHING MORE USER-FRIENDLY.
-    //Node L = null;
-    //int count = 0;
+
       int N = 300;
       if( args.length != 0 )
         N = Integer.parseInt(args[0]);
@@ -26,51 +24,6 @@ public class DotChaser {
           list.moveAll(rand);
         
         }
-    //list.addAll(args);
-    //list.printAll();
-    //list.moveAll(rand);
-    /*while( true ) {
-      // Every N rounds, add another typeA and typeB Thing.
-      if( count % N == 0 ) {
-
-        // Add a typeA thing to the list.
-        // (GEE, THAT'S A LOT OF CODE FOR JUST CREATING ONE THING)
-        Thing tA = new Thing();
-        tA.row = 45;
-        tA.col = 50;
-        Node nA = new Node();
-        nA.data = tA;
-        nA.next = L;
-        L       = nA;
-
-        // Add a typeB thing to the list
-        Thing tB = new Thing();
-        tB.row     = 55;
-        tB.col     = 50;
-        tB.lab     = 'b';
-        tB.isTypeB = true;
-        Node nB = new Node();
-        nB.data = tB;
-        nB.next = L;
-        L       = nB;
-      }
-
-      // Print out each thing.
-      // (SEEMS LIKE A NICE PRINTALL() METHOD CALL WOULD WORK HERE)
-      // (SEEMS LIKE A toString() METHOD IN THE CLASS WOULD ALSO BE NICE)
-      for( Node T = L; T != null; T = T.next )
-        System.out.println(T.data.row + " " + T.data.col + " " + T.data.lab);
-
-      System.out.println("done");
-      System.out.flush();
-
-      // Move each thing.
-      // (SEEMS LIKE A NICE MOVEALL() METHOD CALL WOULD WORK HERE)
-      for( Node T = L; T != null; T = T.next ) {
-        maybeTurn(T.data);
-        step(T.data);
-      }
-      count++;
-    } */
+   
   }
 }
